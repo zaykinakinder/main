f237f0d93c7309958715f9cc5b42cfdb4ab0200b
Yuliya <zaykinakinder@gmail.com>
2022-09-16 23:22:44 +0100

    New prog max num

---
+++ Examples/Example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Hello, What's your name?");
+string username = Console.Readline();
+
+if(username == "Yuliya")
+{
+    Console.WriteLine ("Yay, my favourite!");
+}
+else
+{
+    Console.Write ("Hi,");
+    Console.WriteLine (username);
+}
